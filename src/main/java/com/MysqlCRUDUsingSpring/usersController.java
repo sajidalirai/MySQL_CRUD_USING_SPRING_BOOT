505bed164273d13703eca879cc13ec3fa438a79c
Sajid Rai <31288472+sajidalirai@users.noreply.github.com>
2018-08-27 13:28:01 +0500

    Code Implementation to Prevent DDoS attacks

--- src/main/java/com/MysqlCRUDUsingSpring/usersController.java
+++ src/main/java/com/MysqlCRUDUsingSpring/usersController.java
@@ -3,6 +3,8 @@ package com.MysqlCRUDUsingSpring;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,43 +16,45 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-
 public class usersController {
 	
 	@Autowired
 	usersRepository userpository;
-
+	
 	@GetMapping(value = "/users")
-	public List<users> getAll(){
-		
+	public List<users> getAll(HttpServletResponse response){
+		response.setHeader("Cache-Control", "no-transform, public, max-age=2");
+	    response.setHeader("Pragma", "");
 		return userpository.findAll();
 		
 	}
 	
-	
-	
 	@RequestMapping(value = "/users/{userId}")
-	public Optional<users> UserById(@PathVariable int userId){
-		
+	public Optional<users> UserById(HttpServletResponse response,@PathVariable int userId){
+		response.setHeader("Cache-Control", "no-transform, public, max-age=2");
+	    response.setHeader("Pragma", "");
 		return userpository.findById(userId);
-		
 	}
 
 	@PostMapping(value = "/users")
-	public List<users> persist(@RequestBody users person){
+	public List<users> persist(HttpServletResponse response, @RequestBody users person){
+		response.setHeader("Cache-Control", "no-transform, public, , max-age=1");
+	    response.setHeader("Pragma", "");
 		userpository.save(person);
 		return userpository.findAll();
 	}
 	
 	@RequestMapping(method = RequestMethod.PUT, value = "/users/{userId}")
-	public void updateUser(@RequestBody users person ,@PathVariable int userId ) {
+	public void updateUser(HttpServletResponse response, @RequestBody users person ,@PathVariable int userId ) {
+		response.setHeader("Cache-Control", "no-transform, public, max-age=2");
+	    response.setHeader("Pragma", "");
 		userpository.save(person);
 	}
 	
 	@RequestMapping(method = RequestMethod.DELETE, value = "/users/{userId}")
-	public void deleteUser(@PathVariable int userId) {
+	public void deleteUser(HttpServletResponse response, @PathVariable int userId) {
+		response.setHeader("Cache-Control", "no-transform, public, max-age=2");
+	    response.setHeader("Pragma", "");
 		userpository.deleteById(userId);
 	}
-	
-
 }
